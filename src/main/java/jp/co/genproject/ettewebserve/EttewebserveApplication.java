1e3940569fc160a34c87cae8143e17f83ac34e46
장승현 <victorhyeon92@gmail.com>
2025-05-21 19:27:19 +0900

    Initial commit with Spring Boot base

---
+++ src/main/java/jp/co/genproject/ettewebserve/EttewebserveApplication.java
@@ -0,0 +1,13 @@
+package jp.co.genproject.ettewebserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EttewebserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EttewebserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/genproject/ettewebserve/EttewebserveApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.genproject.ettewebserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EttewebserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
